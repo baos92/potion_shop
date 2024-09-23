5aa1803310898305679b7c7b34b07bd6c103d92a
German <german.alonsoa@gmail.com>
2024-09-23 10:53:44 +0200

    secret commit test

--- config/config.exs
+++ config/config.exs
@@ -61,3 +61,8 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
+
+
+#Authorization=Basic ZGpjOTh1M2ppZWRtaTI4M2V1OTI4OmFiY2RlZjAxMjM0NTY3ODkw
+
+#aws "SecretAccessKey": "rv1xTuMa6Fr1h5ojNW8BsguSGPiqkT7GNUEZZoL"
